b38adbfb3166b6ab038beb93878961ccef6c7a91
andrewgremlich <andrew.gremlich@gmail.com>
2025-09-21 13:30:44 -0600

    chore: basic formatting with biome

--- src/utils/hashTable.ts
+++ src/utils/hashTable.ts
@@ -1,38 +1,41 @@
 export class HashTable<K extends { toString(): string }, V> {
-  public data: Array<Array<[K, V[]]>>
-  private size: number
+  public data: Array<Array<[K, V[]]>>;
+
+  size: number;
 
   constructor(length: number) {
-    this.data = new Array(length)
-    this.size = 0
+    this.data = new Array(length);
+    this.size = 0;
   }
 
   hash(key: K): number {
-    const keyString = key.toString()
+    const keyString = key.toString();
 
-    let hash = 0
+    let hash = 0;
+
     for (let i = 0; i < keyString.length; i++) {
-      hash += keyString.charCodeAt(i)
+      hash += keyString.charCodeAt(i);
     }
-    return hash % this.data.length
+
+    return hash % this.data.length;
   }
 
   set(key: K, value: V): void {
-    const hash = this.hash(key)
+    const hash = this.hash(key);
 
     if (!this.data[hash]) {
-      this.data[hash] = []
-      this.data[hash].push([key, [value]])
+      this.data[hash] = [];
+      this.data[hash].push([key, [value]]);
     } else {
       for (let i = 0; i < this.data[hash].length; i++) {
         if (JSON.stringify(this.data[hash][i][0]) === JSON.stringify(key)) {
-          this.data[hash][i][1].push(value)
-          return
+          this.data[hash][i][1].push(value);
+          return;
         }
       }
-      this.data[hash].push([key, [value]])
+      this.data[hash].push([key, [value]]);
     }
 
-    this.size++
+    this.size++;
   }
 }
